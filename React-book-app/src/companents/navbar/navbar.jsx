71d45d9303347ed9fae657b97d7e6949a0b81ccf
AzizMardonqulov <=>
2025-03-08 23:47:44 +0500

    first commit

---
+++ React-book-app/src/companents/navbar/navbar.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import NavbarLogo from '../../assets/navbar/navbar-logo.svg'
+import './navbar.css'
+import { FaInstagram } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
+import { CiShoppingCart } from "react-icons/ci";
+
+
+
+
+export default function navbar() {
+  return (
+    <div className='Navbar'>
+        <div className="navbar-star">
+            <div className="navbar-logo">
+                <img src={NavbarLogo} alt="" />
+                <div className="navbar-star-item">
+                    <ul>
+                      <li><a href=""><FaInstagram /></a>
+                      </li>
+                      <li><a href=""><FaGithub /></a></li>
+                      <li><a href=""><FaLinkedinIn /></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div className="navbar-left">
+            <div className="navbar-left-1">
+                <div className="left-1">
+                    <ul>
+                      <li><a className='active' href="">Home</a></li>
+                      <li><a href="">About</a></li>
+                      <li><a href="">Shop</a></li>
+                      <li><a href="">Contact</a></li>
+                    </ul>
+                </div>
+                <div className="left-2">
+                    <div className="left-2-item-1">
+                      <a href=""><CiShoppingCart /><span>0</span></a>
+                    </div>
+                    <div className="left-2-item-2">
+                      <a href="">Order Today</a>
+                    </div>
+                </div>
+                <div className="left-3">
+                  <div className="bars">
+                    <span></span>
+                  </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
---
+++ React-book-app/src/page/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Home.css'
+import Navbar  from '../companents/navbar/navbar'
+
+export default function Home() {
+  return (
+    <Navbar />
+  )
+}
