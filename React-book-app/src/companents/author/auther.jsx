c2585c2a30f0a4d3d0c6375dfd088dfc23d98c54
AzizMardonqulov <=>
2025-03-09 22:45:34 +0500

    add auther part by html in home page

---
+++ React-book-app/src/companents/author/auther.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import './auther.css'
+import autherImg1 from "../../assets/auther/1.png"
+import autherImg2 from "../../assets/auther/2.png"
+export default function auther() {
+  return (
+    <div className='auther'>
+        <div className="auther-in">
+            <div className="auther-lable-1">The Author’s Book</div>
+            <div className="auther-page">
+                <div className="auther-page-1">
+                    <div className="auther-page-img">
+                        <img src={autherImg1} alt="" />
+                    </div>
+                    <div className="auther-page-lable">
+                        <a href='#' className="auther-page-lable-1">Atomic One’s</a>
+                        <div className="auther-page-lable-2">$ 13.84 USD</div>
+                        <div className="auther-page-lable-3">As the book contains theoretical content as well as solved questions.</div>
+                        <div className="auther-page-lable-4">Printed Book</div>
+                        <div className="auther-page-button">
+                            <a href="#">Order Now</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="auther-page-1">
+                    <div className="auther-page-img">
+                        <img src={autherImg2} alt="" />
+                    </div>
+                    <div className="auther-page-lable">
+                        <a href='#' className="auther-page-lable-1">The Dark Light</a>
+                        <div className="auther-page-lable-2">$ 13.84 USD</div>
+                        <div className="auther-page-lable-3">As the book contains theoretical content as well as solved questions.</div>
+                        <div className="auther-page-lable-4">Printed Book</div>
+                        <div className="auther-page-button">
+                            <a href="#">Order Now</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
--- React-book-app/src/page/Home.jsx
+++ React-book-app/src/page/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './Home.css'
 import Navbar  from '../companents/navbar/navbar'
 import Baner from '../companents/baner/baner'
+import Auther from '../companents/author/auther'
 
 export default function Home() {
   return (
     <div className="home">
           <Navbar />
           <Baner />
+          <Auther />
     </div>
   )
 }
