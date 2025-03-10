c7423ab6ff4b3611a30ebfaec3141a68cc1d740b
AzizMardonqulov <=>
2025-03-10 17:45:29 +0500

    add article

---
+++ React-book-app/src/companents/article/article.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './article.css'
+import ArticleImg1 from '../../assets/article/3.jpg'
+import ArticleImg2 from '../../assets/article/3.jpg'
+import ArticleImg3 from '../../assets/article/3.jpg'
+
+export default function article() {
+  return (
+    <div className='article'>
+        <div className="article-lable-1">
+        Articles & Resources
+        </div>
+
+    </div>
+  )
+}
--- React-book-app/src/page/Home.jsx
+++ React-book-app/src/page/Home.jsx
@@ -5,6 +5,7 @@ import Baner from '../companents/baner/baner'
 import Auther from '../companents/author/auther'
 import About from '../companents/about/about'
 import Trust from '../companents/trusted/trust'
+import Article from '../companents/article/article'
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
           <Auther />
           <About / >
           <Trust / >
+          <Article / >
     </div>
   )
 }
