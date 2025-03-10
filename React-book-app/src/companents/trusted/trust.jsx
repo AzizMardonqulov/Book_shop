f25bed9badab514bed88292ae9aaab37da09324e
AzizMardonqulov <=>
2025-03-10 13:02:34 +0500

    add trust part-2 in html code in home page

--- React-book-app/src/companents/trusted/trust.jsx
+++ React-book-app/src/companents/trusted/trust.jsx
@@ -4,6 +4,7 @@ import trustImg1 from '../../assets/trust/1.png'
 import trustImg2 from '../../assets/trust/2.png'
 import trustImg3 from '../../assets/trust/3.png'
 import trustImg4 from '../../assets/trust/4.png'
+import trustImg5 from '../../assets/trust/5.png'
 
 export default function trust() {
   return (
@@ -46,6 +47,18 @@ export default function trust() {
                         <div className="trust-part-1-lable-1">Urban Store</div>
                         <div className="trust-part-1-lable-2">They has been helping readers, music lovers, and videophiles find quality material</div>
                     </div>
+                </div>
+            </div>
+            <div className="trust-part-2">
+                <div className="trust-part-2-lable">
+                    <div className="trust-part-2-lable-1">Get Book Copy Today!</div>
+                    <div className="trust-part-2-lable-2">We believe that bookstores are essential to a healthy culture. They’re where authors can connect with readers.</div>
+                    <div className="trust-part-2-button">
+                        <a href="#">Order Today</a>
+                    </div>
+                </div>
+                <div className="trust-part-2-img">
+                    <img src={trustImg5} alt="" />
                 </div>
             </div>
         </div>
