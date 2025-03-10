8bda76f1d1dede5387870f5e0a371d548e70ebd3
AzizMardonqulov <=>
2025-03-10 17:51:15 +0500

    add thml code article part in home page

--- React-book-app/src/companents/article/article.jsx
+++ React-book-app/src/companents/article/article.jsx
@@ -10,7 +10,71 @@ export default function article() {
         <div className="article-lable-1">
         Articles & Resources
         </div>
-
+        <div className="article-blog">
+            <div className="article-blog-1">
+                <div className="article-blog-1-img">
+                <img src={ArticleImg1} alt="Article Image 1"/>
+                </div>
+                <div className="article-blog-1-lable">
+                    <div className="article-blog-1-lable-1">
+                    Significant reading has more info number
+                    </div>
+                    <div className="article-blog-1-lable-2">
+                    Override the digital divide with additional clickthroughs from DevOps for real-time schemas.
+                    </div>
+                    <div className="article-blog-1-bottom">
+                        <div className="article-blog-1-bottom-1">
+                            <a href="">Read More</a>
+                        </div>
+                        <div className="article-blog-1-bottom-2">
+                        October 6, 2021
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="article-blog-1">
+                <div className="article-blog-1-img">
+                <img src={ArticleImg1} alt="Article Image 1"/>
+                </div>
+                <div className="article-blog-1-lable">
+                    <div className="article-blog-1-lable-1">
+                    Significant reading has more info number
+                    </div>
+                    <div className="article-blog-1-lable-2">
+                    Override the digital divide with additional clickthroughs from DevOps for real-time schemas.
+                    </div>
+                    <div className="article-blog-1-bottom">
+                        <div className="article-blog-1-bottom-1">
+                            <a href="">Read More</a>
+                        </div>
+                        <div className="article-blog-1-bottom-2">
+                        October 6, 2021
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="article-blog-1">
+                <div className="article-blog-1-img">
+                <img src={ArticleImg1} alt="Article Image 1"/>
+                </div>
+                <div className="article-blog-1-lable">
+                    <div className="article-blog-1-lable-1">
+                    Significant reading has more info number
+                    </div>
+                    <div className="article-blog-1-lable-2">
+                    Override the digital divide with additional clickthroughs from DevOps for real-time schemas.
+                    </div>
+                    <div className="article-blog-1-bottom">
+                        <div className="article-blog-1-bottom-1">
+                            <a href="">Read More</a>
+                        </div>
+                        <div className="article-blog-1-bottom-2">
+                        October 6, 2021
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
