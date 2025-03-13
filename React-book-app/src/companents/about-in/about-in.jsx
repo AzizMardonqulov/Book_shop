fd77a5810b75b9aed1df281b63c370b22ca20cbd
AzizMardonqulov <=>
2025-03-13 07:33:31 +0500

    About-in style and responsive style in about page

--- React-book-app/src/companents/about-in/about-in.jsx
+++ React-book-app/src/companents/about-in/about-in.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './about-in.css'
 import AboutPart1Img from '../../assets/about/2.jpg'
+import AboutPart1Img2 from '../../assets/about/3.jpg'
 import Logos from '../../assets/about/logos.jpg'
 export default function aboutIn() {
   return (
@@ -34,6 +35,44 @@ export default function aboutIn() {
                         <a href="">Contack Now</a>
                     </div>
                 </div>
+            </div>
+        </div>
+        <div className="about-in-part-2">
+            <div className="about-in-part-2-lable">
+                <div className="about-in-part-2-lable-1">My Story</div>
+                <div className="about-in-part-2-lable-2">We believe that bookstores are essential to a healthy culture. They’re where authors can connect with readers, where we discover new writers, where children get hooked on the thrill of reading.</div>
+                <div className="about-in-part-2-blog-s">
+                    <div className="about-in-part-2-blog-1">
+                        <div className="about-in-part-2-blog-1-num">
+                            01
+                        </div>
+                        <div className="lable-in">
+                        <div className="about-in-part-2-blog-1-lable-1">Great Author Awards - 2006</div>
+                        <div className="about-in-part-2-blog-1-lable-1">Won a best author of the year for my second & fourth book about the science fiction.</div>
+                        </div>
+                    </div>
+                    <div className="about-in-part-2-blog-1">
+                        <div className="about-in-part-2-blog-1-num">
+                            02
+                        </div>
+                        <div className="lable-in">
+                        <div className="about-in-part-2-blog-1-lable-1">Great Author Awards - 2006</div>
+                        <div className="about-in-part-2-blog-1-lable-1">Won a best author of the year for my second & fourth book about the science fiction.</div>
+                        </div>
+                    </div>
+                    <div className="about-in-part-2-blog-1">
+                        <div className="about-in-part-2-blog-1-num">
+                            03
+                        </div>
+                        <div className="lable-in">
+                        <div className="about-in-part-2-blog-1-lable-1">Great Author Awards - 2006</div>
+                        <div className="about-in-part-2-blog-1-lable-1">Won a best author of the year for my second & fourth book about the science fiction.</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="about-in-part-2-img">
+                <img src={AboutPart1Img2} alt="" />
             </div>
         </div>
     </div>
--- React-book-app/src/page/About.jsx
+++ React-book-app/src/page/About.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import Navbar from '../companents/navbar/navbar'
 import AboutBaner from '../companents/About-baner/About-baner';
 import AboutIn from '../companents/about-in/about-in';
+import Auther from '../companents/author/auther'
+import Trust from '../companents/trusted/trust'
+import Article from '../companents/article/article'
+import Futer from '../companents/futer/futer'
 
 export default function About() {
   return (
